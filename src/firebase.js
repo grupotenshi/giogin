fe4b4625c3887844888f2940b8ccaeecec0b55d9
Miguel Tenshi <grupotenshi@gmail.com>
2020-01-21 22:33:45 -0500

    Configuración correcta firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,9 @@
 export default {
-  apiKey: "AIzaSyBJL5ySfS1kbafVNA5kVsAV6N8gJSu7kZQ",
-  authDomain: "healthiergaby1.firebaseapp.com",
-  databaseURL: "https://healthiergaby1.firebaseio.com",
-  projectId: "healthiergaby1",
-  storageBucket: "healthiergaby1.appspot.com",
-  messagingSenderId: "171900124411",
-  appId: "1:171900124411:web:0662afd8691353bb5c045e",
-  measurementId: "G-SGERB97MK3"
+  apiKey: "AIzaSyCMqXWg1eEDkhUUWakaNBg4RCYFzHjoopA",
+  authDomain: "grupo-tenshi.firebaseapp.com",
+  databaseURL: "https://grupo-tenshi.firebaseio.com",
+  projectId: "grupo-tenshi",
+  storageBucket: "grupo-tenshi.appspot.com",
+  messagingSenderId: "253657633287",
+  appId: "1:253657633287:web:f3a8115abc457d0c5690a2"
 }
